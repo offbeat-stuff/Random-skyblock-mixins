0cba60a83f61c850544c059cacea3c776a878e25
Artix Kai <artix@github.com>
2022-12-07 21:23:10 +0530

    change the nylium generation to 10% chance and bedrock parity,,

--- src/main/java/io/github/offbeat_stuff/random_skyblock_mixins/mixin/renewable_netherack_and_nylium/PortalForcerMixin.java
+++ src/main/java/io/github/offbeat_stuff/random_skyblock_mixins/mixin/renewable_netherack_and_nylium/PortalForcerMixin.java
@@ -1,22 +1,16 @@
 package io.github.offbeat_stuff.random_skyblock_mixins.mixin.renewable_netherack_and_nylium;
 
-import java.util.Optional;
-
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.registry.Holder;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.world.BlockLocating;
 import net.minecraft.world.PortalForcer;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
@@ -28,27 +22,17 @@ public class PortalForcerMixin {
   private ServerWorld world;
 
   private BlockState getExtensionBlock(Holder<Biome> biome) {
-    if (biome.isRegistryKey(BiomeKeys.CRIMSON_FOREST))
+    if (biome.isRegistryKey(BiomeKeys.CRIMSON_FOREST) && world.getRandom().nextInt(10) == 0)
       return Blocks.CRIMSON_NYLIUM.getDefaultState();
-    if (biome.isRegistryKey(BiomeKeys.WARPED_FOREST))
+    if (biome.isRegistryKey(BiomeKeys.WARPED_FOREST) && world.getRandom().nextInt(10) == 0)
       return Blocks.WARPED_NYLIUM.getDefaultState();
     return Blocks.NETHERRACK.getDefaultState();
   }
 
-  @Inject(method = "createPortal", at = @At(value = "CONSTANT", args = "intValue=-1", ordinal = 4), locals = LocalCapture.CAPTURE_FAILSOFT)
-  private void changeExtensionBlock(BlockPos pos, Direction.Axis axis,
-      CallbackInfoReturnable<Optional<BlockLocating.Rectangle>> cir, Direction direction, double d, BlockPos blockPos) {
-    var mutable = new BlockPos.Mutable();
-    var dir = direction.rotateYClockwise();
-    for (int i = -1; i < 3; ++i) { // i coordinate parallel to portal
-      for (int j = -2; j < 3; ++j) { // j coordinate perpendicular to portal
-        if ((Math.abs(j) == 1 && (i == -1 || i == 2)) ||
-            (Math.abs(j) == 2 && (i == 0 || i == 1))) {
-          mutable.set(blockPos, direction.getOffsetX() * i + dir.getOffsetX() * j, -1,
-              direction.getOffsetZ() * i + dir.getOffsetZ() * j);
-          world.setBlockState(mutable, getExtensionBlock(world.getBiome(mutable)));
-        }
-      }
-    }
+  @Redirect(method = "createPortal", at = @At(value = "INVOKE", target = "net/minecraft/server/world/ServerWorld.setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z", ordinal = 0))
+  private boolean changeExtensionBlock(ServerWorld world, BlockPos pos, BlockState state) {
+    if (state.isOf(Blocks.OBSIDIAN))
+      return world.setBlockState(pos, getExtensionBlock(world.getBiome(pos)));
+    return world.setBlockState(pos, state);
   }
 }
